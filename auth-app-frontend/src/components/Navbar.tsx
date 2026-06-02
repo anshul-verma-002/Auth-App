bcadcdbf7dcdc0c4909ac11822062fab93f37fb8
Anshul <anshulsen01032006@gmail.com>
2026-06-02 16:01:08 +0530

    Update Navbar.tsx

--- auth-app-frontend/src/components/Navbar.tsx
+++ auth-app-frontend/src/components/Navbar.tsx
@@ -34,7 +34,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="flex flex-col md:flex-row justify-around items-center md:h-14 gap-4 md:gap-0 py-2 md:py-0 dark:bg-slate-950 bg-white border-b dark:border-slate-800 border-slate-200 shadow-sm">
+    <nav className="flex flex-col md:flex-row justify-around items-center md:h-14 gap-3 md:gap-0 py-2 md:py-0 dark:bg-slate-950 bg-white border-b dark:border-slate-800 border-slate-200 shadow-sm pt-4">
       {/* Logo */}
       <div className="font-semibold flex gap-2 items-center">
         <span className="inline-flex items-center justify-center h-7 w-7 rounded-md bg-gradient-to-r from-blue-500 to-violet-500 text-white text-sm shadow-md">
@@ -49,7 +49,7 @@ const Navbar = () => {
       </div>
 
       {/* Nav Links */}
-      <div className="flex items-center justify-around md:gap-4 gap-2">
+      <div className="flex items-center justify-around md:gap-4 gap-5">
         <button onClick={toggleTheme}>
           {darkMode ? (
             <SunIcon size={22} className="text-yellow-400 cursor-pointer" />
